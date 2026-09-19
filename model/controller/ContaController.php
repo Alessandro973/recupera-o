aa2f05ff3108cf25329f1ee57bc619b0528d39cc
Alessandro973 <novaeszx69@gmail.com>
2026-09-19 12:37:21 -0300

    Add ContaController to manage bank accounts

---
+++ model/controller/ContaController.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "../model/ContaModel.php";
+
+
+$conta1 = new ContaBancaria();
+$conta1->setTitular("Alessandro");
+$conta1->depositar(1500);
+
+
+$conta2 = new ContaBancaria();
+$conta2->setTitular("Beatriz");
+$conta2->depositar(2500);
+
+
+$conta3 = new ContaBancaria();
+$conta3->setTitular("Carlos");
+$conta3->depositar(800);
+
+
+$contas = [
+    $conta1,
+    $conta2,
+    $conta3
+];
+
+
+require_once "../view/contas.php";
+
+?>
+
