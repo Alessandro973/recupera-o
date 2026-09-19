3400251a4af155a78ea99c06fafc59388b6fa7ba
Alessandro973 <novaeszx69@gmail.com>
2026-09-19 12:29:59 -0300

    Add discount logic based on quantity

---
+++ desconto_quantidade.php
@@ -0,0 +1,12 @@
+php
+<?php
+
+$quantidade = 7;
+
+if ($quantidade >= 10) {
+    echo "Desconto: 20%";
+} elseif ($quantidade >= 5) {
+    echo "Desconto: 10%";
+} else {
+    echo "Sem desconto";
+}
